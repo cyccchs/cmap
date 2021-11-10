1393342c6f9cc3791c7f79824430cf953ff521f5
chs <IRIS-UBUNTU>
2021-11-10 23:14:44 +0800

    .

--- agent.py
+++ agent.py
@@ -1,7 +1,15 @@
 from network import Retina, GlimpseNetwork, LoactionNetwork, CoreNetwork, SelfAttention, SoftAttention
 
 class Agent:
-    def __init__(self):
-        self.GlimpseNetwork = GlimpseNetwork()
-        self.LocationNetwork = LocationNetwork()
+    def __init__(self, h_g, h_l, glimpse_size, c, input_size, output_size, std):
+        self.GlimpseNetwork = GlimpseNetwork(h_g, h_l, glimpse_size, c)
+        self.LocationNetwork = LocationNetwork(input_size, output_size, std)
+    def next_step(self, x, l_pre, h_t):
+        pass
+
+    def save_model(self):
+        pass
+    def load_model(self):
+        pass
+
         
