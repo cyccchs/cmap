e943273eee4f9cc61bd1f9a752477893ea50d511
chs <cyccchs@gmail.com>
2022-01-03 07:38:59 +0800

    glimpse

--- model.py
+++ model.py
@@ -4,10 +4,9 @@ import networks
 import agent
 
 class RecurrentAttention(nn.Module):
-    def __init__(self, h_g, h_l, glimpse_size, c):
+    def __init__(self, batch_size, h_g, h_l, glimpse_size, c):
         super().__init__()
-        self.init_l = torch.FloatTensor(4,2).uniform_(-1.0,1.0)
+        self.init_l = torch.FloatTensor(batch_size,2).uniform_(-1.0,1.0)
         self.glimpsenn = networks.GlimpseNetwork(h_g, h_l, glimpse_size, c)
     def forward(self, img):
         return(self.glimpsenn(img, self.init_l))
-        
