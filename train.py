c39f0dc163391aa6d325ef6f88cbef1998f0339c
chs <cyccchs@gmail.com>
2022-01-14 00:04:17 +0800

    runable para with batch size & image number issue

--- train.py
+++ train.py
@@ -5,34 +5,41 @@ from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 from collater_nobox import *
 from model import *
+import torch.nn.functional as F
 
 ds = HRSC2016()
 collater = Collater(scales=800)
+batch_size = 2
 
 loader = DataLoader(
     dataset=ds,
-    batch_size=2,
+    batch_size=batch_size,
     num_workers=8,
     collate_fn=collater,
     shuffle=False)
 
-pbar = tqdm(enumerate(loader), total=len(loader))
-model = RecurrentAttention(2, 128, 128, 3, 3, 256, 256, 2, 0.22)
+model = MultiAgentRecurrentAttention(batch_size, 128, 128, 3, 3, 256, 256, 2, 0.22)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+for name, para in model.named_parameters():
+    print(name)
+    print(para.shape)
 
 def reset():
-    init_l = torch.FloatTensor(2, 2).uniform_(-1.0,1.0) #batch size
-    init_h = torch.zeros(2, 256, dtype=torch.float32, requires_grad=True) #lstm size
-    
+    init_l = torch.FloatTensor(batch_size, 2).uniform_(-1.0,1.0) #batch size
+    init_l.requires_grad = True
+    init_h = torch.zeros(batch_size, 256, dtype=torch.float32, requires_grad=True) #lstm size
     return init_l, init_h
-#def alpha_reward(reward, alpha):
-#    for i in 
+
 
 l_t, h_t = reset()
+torch.autograd.set_detect_anomaly(True)
+pbar = tqdm(enumerate(loader), total=len(loader))
 
 for i, (ni,batch) in enumerate(pbar):
     imgs, existence = batch['image'], batch['existence']
     l_list, b_list, log_pi_list = [], [], []
-    R = 0
+    optimizer.zero_grad()
     for i in range(4-1):
         h_t, l_t, b_t, log_pi = model(imgs, h_t, l_t)
         l_list.append(l_t)
@@ -44,10 +51,22 @@ for i, (ni,batch) in enumerate(pbar):
     b_list.append(b_t)
     log_pi_list.append(log_pi)
     
-    #baselines = torch.stack(b_list).transpose(1,0)
-    for i in b_list:
-        torch.stack(i).transpose(1,0)
-    log_pi = torch.stack(log_pi_list).transpose(1,0)
-    predicted = torch.max(log_probs, 1)[1].detach()
-    for i in range(len(existence)):
-        R = R + (predicted[i] == existence[i])
+    baselines = torch.stack(b_list, dim=1) #[agent_num, time_step, batch_size]
+    
+    predicted = torch.max(log_probs, 1)[1]  #indices store in element[1]
+    R = (predicted.detach() == torch.tensor(existence)).float()
+    print(log_probs)
+    print(existence)
+    loss = F.nll_loss(log_probs, torch.tensor(existence))
+    loss.backward()
+    optimizer.step()
+    print('Reward', R)
+
+
+
+
+
+
+
+
+
